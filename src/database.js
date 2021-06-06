e07780d982e002c6a119f287a01318ef10de8e6e
Isaías <isaias@artikaweb.com>
2021-06-06 12:16:41 +0200

    Commit 2

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-const URI='mongodb://localhost/merstack';
+const URI=process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/databasesetest';
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
